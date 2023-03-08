9299f9d3c9e3054aa573d21ecd36ece194be65ab
Amer Shaadouh <as@immosolve.de>
2023-03-08 13:41:57 +0100

    TestCase:     (5, 10) -> 1 .. 4 [5] 6 .. 10  passed     andere methode AMZ-2075

--- pag7.js
+++ pag7.js
@@ -2,48 +2,9 @@ const { objectTypeAnnotation } = require("@babel/types");
 
 function generateOutput(current, total) {
   let output = "";
-  if (current === 1) {
-    output += `[1] `;
-    for (let i = 2; i <= 5; i++) {
-      output += `${i} `;
-    }
-    if (total > 5) {
-      output += `.. ${total}`;
-    }
-  } else if (current === 2) {
-    output += `1 [2] `;
-    for (let i = 3; i <= 5; i++) {
-      output += `${i} `;
-    }
-    if (total > 5) {
-      output += `.. ${total}`;
-    }
-  } else if (current === 3) {
-    output += `1 2 [3] `;
-    for (let i = 4; i <= 5; i++) {
-      output += `${i} `;
-    }
-    if (total > 5) {
-      output += `.. ${total}`;
-    }
-  } else if (current === 4) {
-    output += `1 2 3 [4] `;
-    for (let i = 5; i <= 5 && i <= total; i++) {
-      output += `${i} `;
-    }
-    if (total > 5) {
-      output += `.. ${total}`;
-    }
-  } else {
-    if (current > 2) {
-      output += `1 .. ${current - 1} `;
-    }
-    output += `[${current}]`;
-    if (current < total) {
-      output += ` ${current + 1} .. ${total}`;
-    }
+  if (current === current) {
+    output += `1 .. ${current -1 } [${current}] ${current + 1} .. ${total}`;
   }
-
   return output;
 }
 module.exports = generateOutput;
