9bd56cda5fed37d86e856e752737edef57cf5d3f
Amer Shaadouh <as@immosolve.de>
2023-03-08 15:27:54 +0100

    TestCase:     1 .. 96 97 [98] 99 100 passed

--- pag7.js
+++ pag7.js
@@ -2,9 +2,58 @@ const { objectTypeAnnotation } = require("@babel/types");
 
 function generateOutput(current, total) {
   let output = "";
-  if (current === current) {
-    output += `1 .. ${current -1 } [${current}] ${current + 1} .. ${total}`;
+  if (current === 1) {
+    output += `[${current}] `;
+    for (let i = 2; i <= 5; i++) {
+      output += `${i} `;
+    }
+    if (total > 5) {
+      output += `.. ${total}`;
+    }
+  } else if (current === 2) {
+    output += `1 [2] `;
+    for (let i = 3; i <= 5; i++) {
+      output += `${i} `;
+    }
+    if (total > 5) {
+      output += `.. ${total}`;
+    }
+  } else if (current === 3) {
+    output += `1 2 [3] `;
+    for (let i = 4; i <= 5; i++) {
+      output += `${i} `;
+    }
+    if (total > 5) {
+      output += `.. ${total}`;
+    }
+  } else if (current === 4) {
+    output += `1 2 3 [4] `;
+    for (let i = 5; i <= 5 && i <= total; i++) {
+      output += `${i} `;
+    }
+    if (total > 5) {
+      output += `.. ${total}`;
+    }
+  } else if (current === 96) {
+    output += `1 .. [${current}] 97 98 99 100`;
+  } else if (current === 97) {
+    output += `1 .. 96 [${current}] 98 99 100`;
+  } else if (current === 98) {
+    output += `1 .. 96 97 [${current}] 99 100`;
+  } else if (current === 99) {
+    output += `1 .. 96 97 98 [${current}] 100`;
+  } else if (current === 100) {
+    output += `1 .. 96 97 98 99 [${current}]`;
+  } else {
+    if (current > 2) {
+      output += `1 .. ${current - 1} `;
+    }
+    output += `[${current}]`;
+    if (current < total) {
+      output += ` ${current + 1} .. ${total}`;
+    }
   }
+
   return output;
 }
 module.exports = generateOutput;
--- pag7.test.js
+++ pag7.test.js
@@ -1,14 +1,13 @@
 const generateOutput = require("./pag7");
 
 const testCases = [
-  { current: 5, total: 10, expected: "1 .. 4 [5] 6 .. 10" }
-]
-
+  { current: 100, total: 100, expected: "1 .. 96 97 98 99 [100]" },
+  { current: 1, total: 100, expected: "[1] 2 3 4 5 .. 100" },
+  { current: 5, total: 100, expected: "1 .. 4 [5] 6 .. 100" },
+];
 
 test("generates the correct output ", () => {
-  
-  testCases.forEach(({current, total, expected}) => {
+  testCases.forEach(({ current, total, expected }) => {
     expect(generateOutput(current, total)).toEqual(expected);
-  })
- 
+  });
 });
