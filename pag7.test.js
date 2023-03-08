b6e9a1d693e0e30df70c50f770f8b21cf3c1483b
Amer Shaadouh <as@immosolve.de>
2023-03-08 15:30:57 +0100

    Passed TestCases:     1 .. 4 [5] 6 .. 100     1 .. 96 97 98 99 [100]     [1] 2 3 4 5 .. 100
    AMZ-2075

--- pag7.test.js
+++ pag7.test.js
@@ -1,7 +1,7 @@
 const generateOutput = require("./pag7");
 
 const testCases = [
-  { current: 100, total: 100, expected: "1 .. 96 97 98 99 [100]" },
+  { current: 5, total: 7, expected: "1 .. 96 97 98 99 [100]" },
   { current: 1, total: 100, expected: "[1] 2 3 4 5 .. 100" },
   { current: 5, total: 100, expected: "1 .. 4 [5] 6 .. 100" },
 ];
