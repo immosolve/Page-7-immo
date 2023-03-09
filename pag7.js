f16fec06c9fbb938307f8e2785c6a841a09c1209
Amer Shaadouh <as@immosolve.de>
2023-03-09 14:12:17 +0100

    TestCase:     1 2 [3] 4 5 6 7     1 .. 4 [5] 6 .. 10     1 .. 49 [50] 51 .. 100     1 [2] all passed together AMZ-2075

--- pag7.js
+++ pag7.js
@@ -2,25 +2,30 @@ const { objectTypeAnnotation } = require("@babel/types");
 
 function generateOutput(current, total) {
   let output = "";
-  for (let i = 1; i <= total; i++) {
-    if (i < current) {
-      output += `${i} `;
+  // wenn total kleiner oder gleich 7 ist
+  if (total <= 7) {
+    for (let i = 1; i <= total; i++) {
+      if (i < current) {
+        output += `${i} `;
+      }
+      if (i === current) {
+        output += `[${current}]`;
+      }
+      if (i > current) {
+        output += ` ${i}`;
+      }
     }
-    if (i === current) {
-      output += `[${current}]`;
-    }
-    if (i > current) {
-      output += ` ${i}`;
+  } else {
+    //  wenn current groesser 4 oder total kleiner total - 4
+    if (current > 4 || total < total - 4) {
+      if (current === current) {
+        output += `1 .. ${current - 1} [${current}] ${current + 1} .. ${total}`;
+      }
+    } else{
+      
     }
   }
 
-  // if (current > 4) {
-  //   output += `1 .. ${current - 1} `;
-  // }
-  // output += `[${current}]`;
-  // if (current < total) {
-  //   output += ` ${current + 1} .. ${total}`;
-  // }
   return output;
 }
 
