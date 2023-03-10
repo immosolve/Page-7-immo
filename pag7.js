73b4f173d34c3ef1b73fda0a7678b7cb9f1007c7
Amer Shaadouh <as@immosolve.de>
2023-03-10 14:10:43 +0100

    All TestCases:     "1 2 [3] 4 5 6 7"     "1 [2]"     "1 .. 4 [5] 6 .. 10"     "[1] 2 3 4 5 .. 10"     "1 .. 49 [50] 51 .. 100"     "1 2 3 [4] 5 .. 10"     "1 .. 96 97 98 99 [100]"     passed AMZ-2075

--- pag7.js
+++ pag7.js
@@ -15,32 +15,40 @@ function generateOutput(current, total) {
         output += ` ${i}`;
       }
     }
-  } else {
-    //  wenn current groesser 4 oder total kleiner total - 4
-    if (current > 4 || total < total - 4) {
-      if (current === current) {
-        output += `1 .. ${current - 1} [${current}] ${current + 1} .. ${total}`;
+    return output;
+  }
+
+  //  wenn current groesser 4 und  current  kleiner als  total - 4
+  if (current > 4 && current < total - 4) {
+    return `1 .. ${current - 1} [${current}] ${current + 1} .. ${total}`;
+  }
+
+  if (current <= 4) {
+    for (let i = 1; i <= 5; i++) {
+      if (i === current) {
+        output += `[${current}] `;
+      } else {
+        output += `${i} `;
       }
-    } else {
-      if (current <= 4 || total <= total - 4) {
-        for (let i = 1; i <= 5; i++) {
-          if (i === current) {
-            output += ` [${current}]`;
-          } else {
-            output += ` ${i}`;
-          }
+    }
+    if (total === total) {
+      output += `.. ${total}`;
+    }
+  } else {
+    //  wenn current groesser / gleich total - 4
+    if (current >= total - 4) {
+      output += `1 .. `;
+      for (let i = total - 4; i <= total; i++) {
+        //  i ist gleich total -4 zaehle von i bis total
+        if (current === i) {
+          output += `[${i}] `;
+        }else {
+          output += `${i} `
         }
-        if (total === total) {
-          output += ` .. ${total}`;
-        }
-      // } else {
-      //   if ( total <= total - 4) {
-      //     for (let i = 1; i <= total - 4; i++){}
-        // }
+       
       }
     }
   }
-
   return output;
 }
 
--- pag7.test.js
+++ pag7.test.js
@@ -2,13 +2,13 @@ const generateOutput = require("./pag7");
 
 const testCases = [
   { current: 3, total: 7, expected: "1 2 [3] 4 5 6 7" },
-  { current: 5, total: 10, expected: "1 .. 4 [5] 6 .. 10" },
-  { current: 50, total: 100, expected: "1 .. 49 [50] 51 .. 100" },
   { current: 2, total: 2, expected: "1 [2]" },
-  { current: 1, total: 10, expected: " [1] 2 3 4 5 .. 10" },
-  // { current: 100, total: 100, expected: "1 .. 96 97 98 99 [100]" },
-  // { current: 4, total: 10, expected: "1 2 3 [4] 5 .. 10" },
-  // { current: 7, total: 10, expected: "1 .. 6 [7] 8 9 10" },
+  { current: 5, total: 10, expected: "1 .. 4 [5] 6 .. 10" },
+  { current: 1, total: 10, expected: "[1] 2 3 4 5 .. 10" },
+  { current: 50, total: 100, expected: "1 .. 49 [50] 51 .. 100" },
+  { current: 100, total: 100, expected: "1 .. 96 97 98 99 [100] " },
+  { current: 4, total: 10, expected: "1 2 3 [4] 5 .. 10" },
+  { current: 7, total: 10, expected: "1 .. 6 [7] 8 9 10 " },
 ];
 
 test("generates the correct output ", () => {
